4347390a2ba3e1abac69224615674f5f80f4e622
Nehemiah Tefera <nehemiaha1@outlook.com>
2023-10-06 13:52:31 -0500

    Discovery Server Setup

--- ServiceDiscovery/src/main/java/com/example/ServiceDiscoveryApplication.java
+++ ServiceDiscovery/src/main/java/com/example/ServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Hello world!
  *
  */
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication
 {
     public static void main( String[] args )
