d5cf756e6318b48457c938d698af89af45f71eba
Javokhir Buvanazarov <javokhir.buvanazarov@gmail.com>
2023-10-11 12:24:17 -0500

    fix error

--- frontend/src/pages/auth/signup-page.tsx
+++ frontend/src/pages/auth/signup-page.tsx
@@ -23,29 +23,29 @@ type LoginFields = {
 };
 
 const schema = yup
-  .object({
-    firstname: yup.string().required(),
-    lastname: yup.string().required(),
-    street1: yup.string().required(),
-    street2: yup.string(),
-    city: yup.string().required(),
-    state: yup.string().required(),
-    zipcode: yup.string().required(),
-    email: yup.string().required().email(),
-    password: yup.string().required(),
-    confirmPassword: yup
-      .string()
-      .oneOf([yup.ref("password"), null], "Passwords must match"),
-  })
-  .required();
+    .object({
+      firstName: yup.string().required(),
+      lastName: yup.string().required(),
+      street1: yup.string().required(),
+      street2: yup.string(),
+      city: yup.string().required(),
+      state: yup.string().required(),
+      zipcode: yup.string().required(),
+      email: yup.string().required().email(),
+      password: yup.string().required(),
+      confirmPassword: yup
+          .string()
+          .oneOf([yup.ref("password"), null], "Passwords must match"),
+    })
+    .required();
 
 const SignupPage = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [success, setSuccess] = useState<boolean>(false);
   const [submissionErrors, setSubmissionErrors] = useState<string[]>([]);
   console.log(
-    "🚀 ~ file: signup-page.tsx:32 ~ SignupPage ~ submissionErrors",
-    submissionErrors
+      "🚀 ~ file: signup-page.tsx:32 ~ SignupPage ~ submissionErrors",
+      submissionErrors
   );
 
   const {
@@ -64,7 +64,7 @@ const SignupPage = () => {
   const { ref: emailRef, ...emailRest } = register("email");
   const { ref: passwordRef, ...passwordRest } = register("password");
   const { ref: confirmPasswordRef, ...confirmPasswordRest } =
-    register("confirmPassword");
+      register("confirmPassword");
 
   const onsubmit = async (data: LoginFields) => {
     setSubmissionErrors([]);
@@ -80,11 +80,11 @@ const SignupPage = () => {
       setSuccess(true);
     } catch (error) {
       if (
-        (error as any).response.data.errors &&
-        (error as any).response.data.errors.length > 0
+          (error as any).response.data.errors &&
+          (error as any).response.data.errors.length > 0
       ) {
         const errorsArray = (error as any).response.data.errors.map(
-          (err: { msg: string }) => err.msg
+            (err: { msg: string }) => err.msg
         );
         setSubmissionErrors(errorsArray);
       } else if ((error as any).response.data.message) {
@@ -94,174 +94,175 @@ const SignupPage = () => {
   };
 
   return (
-    <AuthLayout>
-      <form
-        noValidate
-        onSubmit={handleSubmit(onsubmit)}
-        className="bg-white/20"
-      >
-        <h1 className="text-center font-bold text-slate-800 text-3xl mb-4 text-indigo-900">
-          Registration
-        </h1>
-        {submissionErrors.length > 0 ? (
-          <ul className="p-4 border-[1px] border-red-500 rounded-xl mb-5 max-w-sm flex items-center justify-center flex-col space-y-3">
-            {submissionErrors.map((err, i) => (
-              <li key={i} className="text-red-500 font-bold">
-                • {err}.
-              </li>
-            ))}
-          </ul>
-        ) : (
-          ""
-        )}
-        {success ? (
-          <div className="flex flex-col justify-center text-slate-800 items-center space-y-6 mx-auto container">
-            <h1 className="text-3xl font-light">
-              You've successfully signed up!
-            </h1>
-            <p className="text-xl font-light text-center">
-              You can now go and login to your new account!
-            </p>
-            <Link to="/auth/login">
-              <Button
-                value="Go Login!"
-                className="bg-green-500 hover:bg-green-600"
-              />
-            </Link>
-          </div>
-        ) : (
-          <div className="flex flex-col space-y-6">
-            <div className="flex flex-col space-y-3">
-              <ClientInput
-                  placeholder="First name"
-                  reference={firstnameRef}
-                  {...firstnameRest}
-              />
-              {errors.firstName?.message ? (
-                  <FormFieldError errorMessage={errors.firstName.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-3">
-              <ClientInput
-                  placeholder="Last name"
-                  reference={lastnameRef}
-                  {...lastnameRest}
-              />
-              {errors.lastName?.message ? (
-                  <FormFieldError errorMessage={errors.lastName.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-3">
-              <ClientInput
-                placeholder="Email"
-                reference={emailRef}
-                {...emailRest}
-              />
-              {errors.email?.message ? (
-                <FormFieldError errorMessage={errors.email.message} />
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                type={"password"}
-                placeholder="Password"
-                reference={passwordRef}
-                {...passwordRest}
-              />
-              {errors.password?.message ? (
-                <FormFieldError errorMessage={errors.password.message} />
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                type={"password"}
-                placeholder="Confirm Password"
-                reference={confirmPasswordRef}
-                {...confirmPasswordRest}
-              />
-              {errors.confirmPassword?.message ? (
-                <FormFieldError errorMessage={errors.confirmPassword.message} />
-              ) : (
-                ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                  placeholder="Street 1"
-                  reference={street1Ref}
-                  {...street1Rest}
-              />
-              {errors.street1?.message ? (
-                  <FormFieldError errorMessage={errors.street1.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                  placeholder="Street 2"
-                  reference={street2Ref}
-                  {...street2Rest}
-              />
-              {errors.street2?.message ? (
-                  <FormFieldError errorMessage={errors.street2.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                  placeholder="City"
-                  reference={cityRef}
-                  {...cityRest}
-              />
-              {errors.city?.message ? (
-                  <FormFieldError errorMessage={errors.city.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                  placeholder="State"
-                  reference={stateRef}
-                  {...stateRest}
-              />
-              {errors.state?.message ? (
-                  <FormFieldError errorMessage={errors.state.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <div className="flex flex-col space-y-1">
-              <ClientInput
-                  placeholder="Zipcode"
-                  reference={zipcodeRef}
-                  {...zipcodeRest}
-              />
-              {errors.zipcode?.message ? (
-                  <FormFieldError errorMessage={errors.zipcode.message} />
-              ) : (
-                  ""
-              )}
-            </div>
-            <Button className="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
+      <AuthLayout>
+        <form
+            noValidate
+            onSubmit={handleSubmit(onsubmit)}
+            className="bg-white/20"
+        >
+          <h1 className="text-center font-bold text-slate-800 text-3xl mb-4 text-indigo-900">
+            Registration
+          </h1>
+          {submissionErrors.length > 0 ? (
+              <ul className="p-4 border-[1px] border-red-500 rounded-xl mb-5 max-w-sm flex items-center justify-center flex-col space-y-3">
+                {submissionErrors.map((err, i) => (
+                    <li key={i} className="text-red-500 font-bold">
+                      • {err}.
+                    </li>
+                ))}
+              </ul>
+          ) : (
+              ""
+          )}
+          {success ? (
+              <div className="flex flex-col justify-center text-slate-800 items-center space-y-6 mx-auto container">
+                <h1 className="text-3xl font-light">
+                  You've successfully signed up!
+                </h1>
+                <p className="text-xl font-light text-center">
+                  You can now go and login to your new account and get our
+                  hot-special-delivery cookies suggestions!
+                </p>
+                <Link to="/auth/login">
+                  <Button
+                      value="Go Login!"
+                      className="bg-green-500 hover:bg-green-600"
+                  />
+                </Link>
+              </div>
+          ) : (
+              <div className="flex flex-col space-y-6">
+                <div className="flex flex-col space-y-3">
+                  <ClientInput
+                      placeholder="First name"
+                      reference={firstnameRef}
+                      {...firstnameRest}
+                  />
+                  {errors.firstName?.message ? (
+                      <FormFieldError errorMessage={errors.firstName.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <ClientInput
+                      placeholder="Last name"
+                      reference={lastnameRef}
+                      {...lastnameRest}
+                  />
+                  {errors.lastName?.message ? (
+                      <FormFieldError errorMessage={errors.lastName.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-3">
+                  <ClientInput
+                      placeholder="Email"
+                      reference={emailRef}
+                      {...emailRest}
+                  />
+                  {errors.email?.message ? (
+                      <FormFieldError errorMessage={errors.email.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      type={"password"}
+                      placeholder="Password"
+                      reference={passwordRef}
+                      {...passwordRest}
+                  />
+                  {errors.password?.message ? (
+                      <FormFieldError errorMessage={errors.password.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      type={"password"}
+                      placeholder="Confirm Password"
+                      reference={confirmPasswordRef}
+                      {...confirmPasswordRest}
+                  />
+                  {errors.confirmPassword?.message ? (
+                      <FormFieldError errorMessage={errors.confirmPassword.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      placeholder="Street 1"
+                      reference={street1Ref}
+                      {...street1Rest}
+                  />
+                  {errors.street1?.message ? (
+                      <FormFieldError errorMessage={errors.street1.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      placeholder="Street 2"
+                      reference={street2Ref}
+                      {...street2Rest}
+                  />
+                  {errors.street2?.message ? (
+                      <FormFieldError errorMessage={errors.street2.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      placeholder="City"
+                      reference={cityRef}
+                      {...cityRest}
+                  />
+                  {errors.city?.message ? (
+                      <FormFieldError errorMessage={errors.city.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      placeholder="State"
+                      reference={stateRef}
+                      {...stateRest}
+                  />
+                  {errors.state?.message ? (
+                      <FormFieldError errorMessage={errors.state.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <div className="flex flex-col space-y-1">
+                  <ClientInput
+                      placeholder="Zipcode"
+                      reference={zipcodeRef}
+                      {...zipcodeRest}
+                  />
+                  {errors.zipcode?.message ? (
+                      <FormFieldError errorMessage={errors.zipcode.message} />
+                  ) : (
+                      ""
+                  )}
+                </div>
+                <Button className="bg-indigo-500 text-gray-100 p-4 w-full rounded-full tracking-wide
                                 font-semibold font-display focus:outline-none focus:shadow-outline hover:bg-indigo-600
                                 shadow-lg" isLoading={isLoading} value="Register" type="submit">
-              Sing up
-            </Button>
-          </div>
-        )}
-      </form>
-    </AuthLayout>
+                  Sing up
+                </Button>
+              </div>
+          )}
+        </form>
+      </AuthLayout>
   );
 };
 
