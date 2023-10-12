519fdace101d988dfac1b9cd4095054248f83d8d
Nehemiah Tefera <nehemiaha1@outlook.com>
2023-10-12 15:17:36 -0500

    create dummy data in the database

--- UserService/src/main/java/com/example/UserServiceApplication.java
+++ UserService/src/main/java/com/example/UserServiceApplication.java
@@ -26,20 +26,81 @@ public class UserServiceApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        createAdmin();
+        createCustomer();
+        createManager();
+        createTeller();
+    }
+
+    private void createCustomer() {
+        User customer = User.builder()
+                .email("customer@cus.com")
+                .firstName("customer")
+                .lastName("customer")
+                .role(Role.CUSTOMER)
+                .address(Address.builder()
+                        .street1("1000 N 4th St")
+                        .city("Fairfield")
+                        .state("Iowa")
+                        .zip("52557")
+                        .build())
+                .isActive(false)
+                .password("1")
+                .build();
+        userService.saveAdmins(customer);
+    }
+
+    private void createTeller() {
+        User teller = User.builder()
+                .email("customer@cus.com")
+                .firstName("customer")
+                .lastName("customer")
+                .role(Role.CUSTOMER)
+                .address(Address.builder()
+                        .street1("1000 N 4th St")
+                        .city("Fairfield")
+                        .state("Iowa")
+                        .zip("52557")
+                        .build())
+                .isActive(true)
+                .password("1")
+                .build();
+        userService.saveAdmins(teller);
+    }
+
+    private void createAdmin() {
         User admin = User.builder()
                 .email("admin@admin.com")
-                .firstName("admin")
-                .lastName("admin")
+                .firstName("Admin")
+                .lastName("Admin")
                 .role(Role.ADMIN)
                 .address(Address.builder()
-                                .street1("1000 N 4th St")
-                                .city("Fairfield")
-                                .state("Iowa")
-                                .zip("52557")
-                                .build())
+                        .street1("1000 N 4th St")
+                        .city("Fairfield")
+                        .state("Iowa")
+                        .zip("52557")
+                        .build())
                 .isActive(true)
-                .password("admin")
+                .password("1")
                 .build();
         userService.saveAdmins(admin);
+    }
+
+    private void createManager() {
+        User manager = User.builder()
+                .email("manager@man.com")
+                .firstName("Manager")
+                .lastName("Manager")
+                .role(Role.MANAGER)
+                .address(Address.builder()
+                        .street1("1000 N 4th St")
+                        .city("Fairfield")
+                        .state("Iowa")
+                        .zip("52557")
+                        .build())
+                .isActive(true)
+                .password("1")
+                .build();
+        userService.saveAdmins(manager);
     }
 }
