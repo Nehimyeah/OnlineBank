3ec22b11e7845b6eec82821d7ae82bcf5f78eee9
Nehemiah Tefera <nehemiaha1@outlook.com>
2023-10-13 19:34:04 -0500

    create necessary inheritance dto

---
+++ BankServiceFinal/src/main/java/com/example/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.example.service;
+
+import com.example.domain.Account;
+import com.example.dto.AccountDto;
+
+public interface AccountService<E extends Account, D extends AccountDto> {
+
+
+}
