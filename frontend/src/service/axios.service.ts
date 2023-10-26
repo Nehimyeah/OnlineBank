898f3e79822971b3750f51f2c9495739e7a60428
Javokhir Buvanazarov <javokhir.buvanazarov@gmail.com>
2023-10-25 21:29:29 -0500

    changed

--- frontend/src/service/axios.service.ts
+++ frontend/src/service/axios.service.ts
@@ -20,6 +20,10 @@ export const axiosPrivateBank = axios.create({
   baseURL: BACKEND_URL ?? "http://localhost:8181",
 });
 
+export const axiosPrivateReport = axios.create({
+    baseURL: BACKEND_URL ?? "http://localhost:7777",
+});
+
 axiosPrivate.interceptors.request.use(
     function(config) {
       const token = Cookie.get("token");
@@ -56,5 +60,18 @@ axiosPrivateBank.interceptors.request.use(
     },
     function(error) {
       return Promise.reject(error);
+    }
+);
+
+axiosPrivateReport.interceptors.request.use(
+    function(config) {
+        const token = Cookie.get("token");
+        if (token) {
+            config.headers["Authorization"] = 'Bearer ' + token;
+        }
+        return config;
+    },
+    function(error) {
+        return Promise.reject(error);
     }
 );
